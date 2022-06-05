1530281a6f62baf8db42cf8db9fb77c27104c465
Djordje Zlatanovic <zlatanovic.basketball@gmail.com>
2022-06-06 00:26:06 +0200

    End of section 3

---
+++ client/src/features/catalog/Catalog.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@mui/material";
+import { useState, useEffect } from "react";
+import { Product } from "../../app/layout/models/product"
+import ProductList from "./ProductList";
+
+interface Props {
+  products: Product[];
+  addProduct: () => void;
+}
+
+export default function Catalog({products, addProduct}: Props){
+    return (
+        <>
+            <ProductList products={products} />
+            <Button variant='contained' onClick={addProduct}>Add Product</Button>
+        </>
+        
+    )
+}
