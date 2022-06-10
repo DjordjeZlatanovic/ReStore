ce43d7c9558ace0c18c4857a0ac5047df7ce588d
Djordje Zlatanovic <zlatanovic.basketball@gmail.com>
2022-06-10 23:18:40 +0200

    End of section 4

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,37 +1,40 @@
-import { CssBaseline, Container } from "@mui/material";
-import { useEffect, useState } from "react";
+import { CssBaseline, Container, createTheme, ThemeProvider } from "@mui/material";
+import { useState } from "react";
+import { Route } from "react-router-dom";
+import AboutPage from "../../features/about/AboutPage";
 import Catalog from "../../features/catalog/Catalog";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import ContactPage from "../../features/contact/ContactPage";
+import HomePage from "../../features/home/HomePage";
 import Header from "./Header";
-import { Product } from "./models/product";
 function App() {
-  const[products, setProducts] = useState<Product[]>( [] );
-  
-  useEffect(() => {
-    fetch('http://localhost:5000/api/products')
-    .then(response => response.json())
-    .then(data => setProducts(data));
-  }, [])
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? 'dark' : 'light';
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        default: paletteType === 'light' ? '#eaeaea' : '#121212'
+      }
+    }
+  })
 
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
+  }
 
-function addProduct() {
-  setProducts(prevState => [...prevState,
-     {
-       id: prevState.length +101,
-       name: 'product' + (prevState.length + 1), 
-       price: (prevState.length*100) + 100,
-       brand: 'some brand',
-       description: 'some description',
-       pictureUrl: 'http://picsum.photos/200'
-      }])
-}
   return (
-    <>
+    <ThemeProvider theme={theme}>
     <CssBaseline />
-      <Header />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
       <Container>
-         <Catalog products={products} addProduct={addProduct}/>
+         <Route exact path='/' component={HomePage} />
+         <Route exact path='/catalog' component={Catalog} />
+         <Route path='/catalog/:id' component={ProductDetails} />
+         <Route path='/about' component={AboutPage} />
+         <Route path='/contact' component={ContactPage} />
       </Container>
-      </>
+      </ThemeProvider>
    );
   }
   
